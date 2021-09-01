84ecc162caef7c26c66690fcf09af589434726c5
georgeh <georgeh@sfu.ca>
2021-09-01 15:17:18 -0700

    save changes

--- LB+-Tree/common/lbtree.cc
+++ LB+-Tree/common/lbtree.cc
@@ -1631,15 +1631,15 @@ Again2: // find and lock next sibling if necessary
     lp->lock = 0;
     if (scanned < scan_size && np) // keep scanning
     {
-        lp = np;
-        mask = (unsigned int)(lp->bitmap);
+        mask = (unsigned int)(np->bitmap);
         while (mask) {
             jj = bitScan(mask)-1;  // next candidate
-            // results[scanned++] = lp->ent[jj];
-            memcpy(results + scanned, &lp->ent[jj], sizeof(IdxEntry));
-            scanned ++;
+            results[scanned++] = lp->ent[jj];
+            // memcpy(results + scanned, &lp->ent[jj], sizeof(IdxEntry));
+            // scanned ++;
             mask &= ~(0x1<<jj);  // remove this bit
         } // end while
+        lp = np;
         // goto Again2;
     }
     // qsort(results, scanned, sizeof(IdxEntry), lbtree::compareFunc);
