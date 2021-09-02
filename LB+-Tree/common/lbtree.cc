6d39913ca03188e7b99d6d4e98a6449a2eadd216
georgeh <georgeh@sfu.ca>
2021-09-01 20:06:23 -0700

    save changes

--- LB+-Tree/common/lbtree.cc
+++ LB+-Tree/common/lbtree.cc
@@ -1595,7 +1595,8 @@ Again1: // find target leaf and lock it
         _xabort(2);
         goto Again1;
     }
-    ((bnode*)lp)->lock() = 1;
+    // ((bnode*)lp)->lock() = 1;
+    lp->lock = 1;
     // 4. RTM commit
     _xend();
 
