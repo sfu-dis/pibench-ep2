a94d6bc3bc3e746bc90a289ba5ca38fe0d4ea1ef
georgeh <georgeh@sfu.ca>
2021-10-10 19:20:55 -0700

    save changes

--- LB+-Tree/common/lbtree.cc
+++ LB+-Tree/common/lbtree.cc
@@ -619,7 +619,7 @@ Again:
     {
         // NODE_PREF(p);
         // if the lock bit is set, abort
-        if (p->lock())
+        if ((bnodeMeta *)(p->ent)->lock != 0)
         {
             _xabort(1);
             goto Again;
