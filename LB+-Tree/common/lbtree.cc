78ee07d81fb4157d1c656a25f8f1363481021a4a
georgeh <georgeh@sfu.ca>
2021-10-10 19:08:01 -0700

    save changes

--- LB+-Tree/common/lbtree.cc
+++ LB+-Tree/common/lbtree.cc
@@ -618,12 +618,12 @@ Again:
     for (i = tree_meta->root_level; i > 0; i--)
     {
         // NODE_PREF(p);
-        // // if the lock bit is set, abort
-        // if (p->lock())
-        // {
-        //     _xabort(1);
-        //     goto Again;
-        // }
+        // if the lock bit is set, abort
+        if (p->lock())
+        {
+            _xabort(1);
+            goto Again;
+        }
         
         t = p->num();
         // sequential search (which is slightly faster now)
