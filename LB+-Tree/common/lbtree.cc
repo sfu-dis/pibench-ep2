4a70327f3c103aba5f0de1f657b67ec83bd0c512
georgeh <georgeh@sfu.ca>
2021-10-12 16:47:53 -0700

    save changes

--- LB+-Tree/common/lbtree.cc
+++ LB+-Tree/common/lbtree.cc
@@ -666,7 +666,7 @@ Again1: // find target leaf and lock it
     __m128i key_16B = _mm_set1_epi8((char)key_hash);
     __m128i fgpt_16B = _mm_load_si128((const __m128i *)lp);
     __m128i cmp_res = _mm_cmpeq_epi8(key_16B, fgpt_16B);
-    unsigned int mask = (unsigned int)_mm_movemask_epi8(cmp_res); // 1: same; 0: diff
+    mask = (unsigned int)_mm_movemask_epi8(cmp_res); // 1: same; 0: diff
     mask = (mask >> 2) & ((unsigned int)(lp->bitmap));
     pos = -1;
     while (mask)
