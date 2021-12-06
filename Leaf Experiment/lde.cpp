5ae325c2af404343301a233dcab6adec743fbb69
georgeh <georgeh@sfu.ca>
2021-12-05 16:33:09 -0800

    save changes

---
+++ Leaf Experiment/lde.cpp
@@ -0,0 +1,28 @@
+#include "lde.h"
+
+valtype leafnode::find(keytype k)
+{
+#ifdef INDIR_ARR
+	
+#elif defined()
+}
+
+bool leafnode::insert(KV kv)
+{
+
+}
+
+bool leafnode::update(KV kv)
+{
+
+}
+
+bool leafnode::del(keytype k)
+{
+
+}
+
+int leafnode::scan(KV* addr, int size = 0)
+{
+
+}
--- Leaf Experiment/lde.h
+++ Leaf Experiment/lde.h
@@ -3,16 +3,25 @@
 #include <libpmemobj.h>
 
 
-
+#define PMEM
 
 #define LEAF_SIZE 14 
 
-
 #define FINGERPRINT
+#ifdef FINGERPRINT
+	#define SIMD
+#endif
 
 #define UNSORTED_LEAF
+#ifdef UNSORTED_LEAF
+	#define INDIR_ARR
+#else
+	#define LINEAR_SEARCH
+#endif
 
-#define NUM_NEXT_PTR 2
+#define NUM_NEXT_PTR 1
+
+#define ALIGNMENT 256
 
 typedef uint64_t keytype;
 typedef uint64_t valtype;
@@ -32,13 +41,29 @@ public:
 
 #ifdef UNSORTED_LEAF
 	bmptype bitmap;
+	#ifdef INDIR_ARR
+		unsigned char iarr[LEAF_SIZE];
+	#endif
+#else
+	unsigned char numKey; // change to unsigned short or int if LEAF_SIZE exceeds 255
 #endif
 
 #ifdef FINGERPRINT
     fgpttype fgpt[LEAF_SIZE]; /* fingerprints */
 #endif
+
     KV ent[LEAF_SIZE];
 
-
     leafnode * next[NUM_NEXT_PTR];
+
+public:
+	valtype find(keytype k); // return valid v if k is found
+
+	bool insert(KV kv);	// return true if kv successfully inserted into leaf, false if kv.k already exists
+
+	bool update(KV kv); // return true if kv successfully updated, false if kv.k does not exist
+
+	bool del(keytype k); // return false if k not found
+
+	int scan(KV* addr, int size); // return number of KV scanned, scanned KVs will be copied to addr
 }
