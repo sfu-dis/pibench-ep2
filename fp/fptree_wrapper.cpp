d657ff9dd1ee2cf278506af3ca8018a5db10f7ec
georgeh <georgeh@sfu.ca>
2022-03-28 18:56:36 -0700

    test fp

---
+++ fp/fptree_wrapper.cpp
@@ -0,0 +1,21 @@
+#include "fptree_wrapper.hpp"
+
+size_t key_size_ = 0;
+size_t pool_size_ = ((size_t)(1024 * 1024 * 16) * 1000);
+const char *pool_path_;
+
+extern "C" tree_api* create_tree(const tree_options_t& opt)
+{
+    auto path_ptr = new std::string(opt.pool_path);
+    if (*path_ptr != "")
+    	pool_path_ = (*path_ptr).c_str();
+    else
+		pool_path_ = "./pool";
+    if (opt.pool_size != 0)
+    	pool_size_ = opt.pool_size;
+
+    printf("PMEM Pool Path: %s\n", pool_path_);
+    printf("PMEM Pool size: %lld\n", pool_size_);
+
+    return new fptree_wrapper();
+}
---
+++ fp/fptree_wrapper.hpp
@@ -0,0 +1,84 @@
+#ifndef __FPTREE_WRAPPER_HPP__
+#define __FPTREE_WRAPPER_HPP__
+
+#include "tree_api.hpp"
+#include "fptree.h"
+
+#include <cstring>
+#include <mutex>
+#include <shared_mutex>
+#include <libpmemobj.h>
+
+// #define DEBUG_MSG
+
+class fptree_wrapper : public tree_api
+{
+public:
+    fptree_wrapper();
+    virtual ~fptree_wrapper();
+    
+    virtual bool find(const char* key, size_t key_sz, char* value_out) override;
+    virtual bool insert(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
+    virtual bool update(const char* key, size_t key_sz, const char* value, size_t value_sz) override;
+    virtual bool remove(const char* key, size_t key_sz) override;
+    virtual int scan(const char* key, size_t key_sz, int scan_sz, char*& values_out) override;
+
+private:
+    fptree_t * tree_;
+};
+
+thread_local char k[128];
+
+fptree_wrapper::fptree_wrapper()
+{
+    tree_ = fptree_create();
+    openPmemobjPool(pool_path_);
+}
+
+fptree_wrapper::~fptree_wrapper()
+{
+    fptree_destroy(tree_);
+}
+
+bool fptree_wrapper::find(const char* key, size_t key_sz, char* value_out)
+{
+    bool found = fptree_get(tree_, *reinterpret_cast<uint64_t*>(const_cast<char*>(key)), (void*)(value_out));
+    if (!found) 
+        printf("Key %llu not found!", *reinterpret_cast<uint64_t*>(const_cast<char*>(key)));
+    return found;
+}
+
+
+bool fptree_wrapper::insert(const char* key, size_t key_sz, const char* value, size_t value_sz)
+{
+    if (!fptree_put(tree_, *reinterpret_cast<uint64_t*>(const_cast<char*>(key)), *reinterpret_cast<uint64_t*>(const_cast<char*>(value))))
+    {
+	#ifdef DEBUG_MSG
+	    printf("Insert failed\n");
+	#endif
+        return false;
+    }
+    return true;
+}
+
+bool fptree_wrapper::update(const char* key, size_t key_sz, const char* value, size_t value_sz)
+{
+    return true;
+}
+
+bool fptree_wrapper::remove(const char* key, size_t key_sz)
+{
+    return true;
+}
+
+int fptree_wrapper::scan(const char* key, size_t key_sz, int scan_sz, char*& values_out)
+{
+	// For now only support 8 bytes key and value (uint64_t)
+    constexpr size_t ONE_MB = 1ULL << 20;
+    static thread_local char results[ONE_MB];
+#ifdef DEBUG_MSG
+    printf("%d records scanned\n", scanned);
+#endif
+    return 0;
+}
+#endif
